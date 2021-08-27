c73f7dba9600a86c30cea1fc00117a0756923381
Ruben Gomez <rubncal.95@gmail.com>
2021-08-27 15:15:48 -0500

    ejercicio de fizz y buzz en platzi

---
+++ proyecto1/curso de programación básica platzi/fizzbuzz/modul.js
@@ -0,0 +1,34 @@
+var number = 100;
+
+for(var i = 1; i <= 100; i ++)
+{
+  if(divisible(i,3))
+  {
+    document.write ("Fizz");
+  }
+
+  if(divisible(i,5))
+  {
+    document.write("Buzz");
+  }
+
+  if(!divisible(i,3) && !divisible(i,5))
+{
+  document.write(i);
+}
+
+  document.write("<br />")
+
+}
+
+function divisible(num, divisor)
+{
+  if(num % divisor == 0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
